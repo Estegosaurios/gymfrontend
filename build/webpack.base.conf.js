6d99aad732833a57e4f8e4c0fef87748039bd137
sergiga <srgarcia91@gmail.com>
2018-10-07 17:58:01 +0200

    Initial vue config added

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: resolve('src/main.js'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      '@': resolve('src'),
+      'store': resolve('src/store'),
+      'router': resolve('src/router')
+    }
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        include: [resolve('src')],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [resolve('src')]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: resolve('dist/index.html'),
+      template: resolve('index.html'),
+      inject: true
+    })
+  ],
+  devServer: {
+    contentBase: resolve('dist'),
+    compress: true,
+    port: 8080,
+    hot: true
+  }
+}
