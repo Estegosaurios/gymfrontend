dc472ad37855bd5e04e9d103995f7b590bc0e9c4
sergiga <srgarcia91@gmail.com>
2018-10-12 16:42:40 +0200

    Created VCard component

---
+++ src/core/VCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card border-0 shadow-sm">
+    <div class="card-body">
+      <h5 class="card-title pb-5">{{ title }}</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- src/programs/ProgramsOverview.vue
+++ src/programs/ProgramsOverview.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="container-fluid py-3">
-    <div
+  <div class="container-fluid">
+    <card
       v-for="program in programs"
       :key="program.id"
-      class="py-5"
-    >{{ program.name }}</div>
+      class="mt-3"
+      :title="program.name"
+    />
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
+import Card from '@/core/VCard';
 
 export default {
+  components: {
+    Card
+  },
   computed: {
     ...mapGetters({
       programs: 'getPrograms'
